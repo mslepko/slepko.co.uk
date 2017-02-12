1b70b3817a99deebe96abb9e91e526363438c9d2
Michal Slepko <michal.slepko@gmail.com>
2017-02-12 21:39:29 +0000

    Clean dist folder before building

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var minifyInline = require('gulp-minify-inline');
 var minifyhtml = require('gulp-minify-html');
+var del = require('del');
 
 gulp.task('minify-css-inline', function() {
   gulp.src('src/*.html')
@@ -19,4 +20,11 @@ gulp.task('minify-html', function () {
     .pipe(gulp.dest("dist/"))
 });
 
-gulp.task('default', ['minify-css-inline', 'minify-html']);
+gulp.task('clean', function() {
+	del(['dist/*.html']).then(paths => {
+    	console.log('Deleted files and folders:\n', paths.join('\n'));
+	});
+
+})
+
+gulp.task('default', ['clean', 'minify-css-inline', 'minify-html']);
