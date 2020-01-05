e994f130429eee8aa4dfa645256a3b6580276d3c
Michal Slepko <michal.slepko@gmail.com>
2020-01-05 22:44:10 +0000

    Added google verification and robots files

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,37 @@ var gulp = require('gulp');
 var minifyInline = require('gulp-minify-inline');
 var minifyhtml = require('gulp-minify-html');
 var del = require('del');
+var copy = require('gulp-copy');
 
 gulp.task('minify-css-inline', function() {
-  gulp.src('src/*.html')
-    .pipe(minifyInline())
-    .pipe(gulp.dest('dist/'))
+    gulp.src('src/*.html')
+        .pipe(minifyInline())
+        .pipe(gulp.dest('dist/'))
+
+    gulp.src('dista/robots.txt')
+        .pipe(gulp.dest("dist/"))
 });
 
-gulp.task('minify-html', function () {
-  var opts = {
-    empty: true,
-    comments: true
-  };
+gulp.task('minify-html', function() {
+    var opts = {
+        empty: true,
+        comments: true
+    };
 
-  gulp.src('dist/*.html')
-    .pipe(minifyhtml(opts))
-    .pipe(gulp.dest("dist/"))
+    gulp.src('dist/*.html')
+        .pipe(minifyhtml(opts))
+        .pipe(gulp.dest("dist/"))
 });
 
 gulp.task('clean', function() {
-	del(['dist/*.html']).then(function(paths) {
-    	console.log('Deleted files and folders:\n', paths.join('\n'));
-	});
-
+    del(['dist/*.*']).then(function(paths) {
+        console.log('Deleted files and folders:\n', paths.join('\n'));
+    });
 })
 
-gulp.task('default', ['clean', 'minify-css-inline', 'minify-html']);
+gulp.task('copy-robots', function() {
+    return gulp.src('./src/robots.txt')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['clean', 'minify-css-inline', 'minify-html', 'copy-robots']);
