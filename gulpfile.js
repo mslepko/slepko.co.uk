f3235590ebcad3faa3a6b2652c39b186324582ae
Michal Slepko <michal.slepko@gmail.com>
2020-01-10 21:47:46 +0000

    Fixed indentation in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,34 +5,31 @@ var del = require('del');
 var copy = require('gulp-copy');
 
 gulp.task('minify-css-inline', function() {
-    gulp.src('src/*.html')
-        .pipe(minifyInline())
-        .pipe(gulp.dest('dist/'))
-
-    gulp.src('dista/robots.txt')
-        .pipe(gulp.dest("dist/"))
+  gulp.src('src/*.html')
+    .pipe(minifyInline())
+    .pipe(gulp.dest('dist/'))
 });
 
 gulp.task('minify-html', function() {
-    var opts = {
-        empty: true,
-        comments: true
-    };
+  var opts = {
+    empty: true,
+    comments: true
+  };
 
-    gulp.src('dist/*.html')
-        .pipe(minifyhtml(opts))
-        .pipe(gulp.dest("dist/"))
+  gulp.src('dist/*.html')
+    .pipe(minifyhtml(opts))
+    .pipe(gulp.dest("dist/"))
 });
 
 gulp.task('clean', function() {
-    del(['dist/*.*']).then(function(paths) {
-        console.log('Deleted files and folders:\n', paths.join('\n'));
-    });
+  del(['dist/*.*']).then(function(paths) {
+    console.log('Deleted files and folders:\n', paths.join('\n'));
+  });
 })
 
 gulp.task('copy-robots', function() {
-    return gulp.src('./src/robots.txt')
-        .pipe(gulp.dest('./dist'));
+  return gulp.src('./src/robots.txt')
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('default', ['clean', 'minify-css-inline', 'minify-html', 'copy-robots']);
